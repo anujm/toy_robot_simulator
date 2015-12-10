c6884fe25d6e4f967be9c2c9884083ffd4b062a0
anujmathur <anuj@mavenhive.in>
2015-12-11 03:28:03 +0530

    Allow robot to turn left

--- lib/robot.rb
+++ lib/robot.rb
@@ -4,4 +4,16 @@ class Robot
   def place(position)
     @position = position
   end
+
+  def left
+    if @position.direction == Direction::NORTH
+      @position = Position.new(position.point, Direction::WEST)
+    elsif @position.direction == Direction::EAST
+      @position = Position.new(position.point, Direction::NORTH)
+    elsif @position.direction == Direction::SOUTH
+      @position = Position.new(position.point, Direction::EAST)
+    elsif @position.direction == Direction::WEST
+      @position = Position.new(position.point, Direction::SOUTH)
+    end
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -17,4 +17,62 @@ describe Robot do
       expect(robot.position.direction).to eq(direction)
     end
   end
+
+  describe '#left' do
+    context 'when facing north' do
+      it 'changes robot direction to west' do
+        point = Point.new(1, 2)
+        direction = Direction::NORTH
+
+        robot = Robot.new
+        robot.place(Position.new(point, direction))
+        robot.left
+
+        expect(robot.position.point).to eq(point)
+        expect(robot.position.direction).to eq(Direction::WEST)
+      end
+    end
+
+    context 'when facing east' do
+      it 'changes robot direction to north' do
+        point = Point.new(1, 2)
+        direction = Direction::EAST
+
+        robot = Robot.new
+        robot.place(Position.new(point, direction))
+        robot.left
+
+        expect(robot.position.point).to eq(point)
+        expect(robot.position.direction).to eq(Direction::NORTH)
+      end
+    end
+
+    context 'when facing south' do
+      it 'changes robot direction to east' do
+        point = Point.new(1, 2)
+        direction = Direction::SOUTH
+
+        robot = Robot.new
+        robot.place(Position.new(point, direction))
+        robot.left
+
+        expect(robot.position.point).to eq(point)
+        expect(robot.position.direction).to eq(Direction::EAST)
+      end
+    end
+
+    context 'when facing west' do
+      it 'changes robot direction to south' do
+        point = Point.new(1, 2)
+        direction = Direction::WEST
+
+        robot = Robot.new
+        robot.place(Position.new(point, direction))
+        robot.left
+
+        expect(robot.position.point).to eq(point)
+        expect(robot.position.direction).to eq(Direction::SOUTH)
+      end
+    end
+  end
 end
